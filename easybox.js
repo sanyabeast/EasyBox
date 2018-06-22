d9676cc02b46a82f265a779a908c767c3116f8e7
sanyabeast <a.gvrnsk@gmail.com>
2018-06-22 17:31:26 +0300

    umd

--- easybox.js
+++ easybox.js
@@ -1,37 +1,11 @@
-!function(e,o){if("function"==typeof define&&define.amd)define(o);else if("object"==typeof module&&module.exports)module.exports=o();else{var n=new(o());window.clavis=n}}(0,function(){
-	window.Class = window.Class || function(name, description){
-		if (typeof name == "object"){
-			description = name;
-			name = "Class";
-		}
-
-		if (typeof description !== "object" || Array.isArray(description)){
-			throw new Error("Invalid class description");
-		}
-
-		var _constructor = description.$constructor;
-		delete description.$constructor;
-		var $constructor = eval([
-			"var " + name + " = function(){",
-			"	if (_constructor) _constructor.apply(this, arguments);",
-			"};" + name
-		].join(""));
-		$constructor.prototype = Object.create(description);
-		return $constructor;
-	};
-
-
+window.Class=window.Class||function(name,description){if("object"==typeof name&&(description=name,name="Class"),"object"!=typeof description||Array.isArray(description))throw new Error("Invalid class description");var _constructor=description.constructor;delete description.constructor;var constructor=eval(["var "+name+" = function(){","\tif (_constructor) _constructor.apply(this, arguments);","};"+name].join(""));return constructor.prototype=Object.create(description).__proto__,constructor},function(o,e){if("function"==typeof define&&define.amd)define(e);else if("object"==typeof module&&module.exports)module.exports=e();else{var t=new(e());window.clavis=t}}(0,function(){
+    
 	var EasyBox = Class("EasyBox", {
 		constructor : function(){
-
-		},
-		privet : function(){
 
 		}
 	});
 
-
-    return EasyBox;
-
+	return EasyBox;
 
 });
